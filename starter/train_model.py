e1861ba5e853bc1c81585c937c4d09c6aab9b976
Majed Aljalajel <majed1@gmail.com>
2022-07-13 14:40:48 +0300

    Implement train and slice models scripts

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,28 +1,45 @@
 # Script to train machine learning model.
+import os
+import joblib
+import pandas as pd
 
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model, inference, compute_model_metrics
 
-# Add code to load in the data.
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+if __name__ == '__main__':
+    
+    file_path = os.path.dirname(__file__)
+    # Add code to load in the data.
+    data = pd.read_csv(os.path.join(file_path, "../data/census.csv"))
+    # Optional enhancement, use K-fold cross validation instead of a train-test split.
+    train, test = train_test_split(data, test_size=0.20, random_state=42)
 
-# Proces the test data with the process_data function.
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-# Train and save a model.
+    # Proces the test data with the process_data function.
+    X_test, y_test, encoder, lb = process_data(
+        test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+    )
+
+    # Train and save a model.
+    trained_model = train_model(X_train, y_train)
+    joblib.dump(trained_model, os.path.join(file_path, '../model/model.joblib'))
+    joblib.dump(encoder, os.path.join(file_path,'../model/encoder.joblib'))
+    joblib.dump(lb, os.path.join(file_path,'../model/lb.joblib'))
